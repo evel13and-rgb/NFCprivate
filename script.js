eabc1536a9ee14f0ae06387dae9d3c819fb12283
Evelin Andrade <evel13and@gmail.com>
2026-08-25 21:40:33 +0000

    Naturaliza el brillo de las luciérnagas

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ import {
   getQuoteBoundaryDecoration,
 } from './quoteOriginal.js?v=4';
 import { EMERGENCY_QUOTES, findStoredQuoteIndex, loadPublicQuotes } from './publicQuotes.js?v=5';
-import { initFireflyAura } from './fireflies.js?v=4';
+import { initFireflyAura } from './fireflies.js?v=5';
 import { getTimeOfDay, isNightTime } from './dayNight.js';
 import { initDaylightMotes, setDaylightMotesActive } from './dayMotes.js';
 import {
